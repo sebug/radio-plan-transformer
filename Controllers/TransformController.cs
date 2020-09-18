ac982a4ea480022dede743d197591b99a49a8cf5
sebug <sebastian.gfeller@gmail.com>
2020-09-18 11:48:20 +0200

    Adding basic controller

---
+++ Controllers/TransformController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace radio_plan_transformer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransformController : ControllerBase
+    {
+        public bool Post()
+        {
+            return true;
+        }
+    }
+}
