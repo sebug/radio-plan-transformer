c7032f7bf00115dee0961b9373091013f2254deb
sebug <sebastian.gfeller@gmail.com>
2020-09-18 12:14:35 +0200

    Adding additional fields

--- Models/TransformRequest.cs
+++ Models/TransformRequest.cs
@@ -5,6 +5,11 @@ namespace radio_plan_transformer.Models
 {
     public class TransformRequest
     {
+        public string CourseType { get; set; }
+        public string CourseNumber { get; set; }
+        public string CourseDates { get; set; }
+        public string CreatedBy { get; set; }
+        public string Phone { get; set; }
         public IFormFile Template { get; set; }
     }
 }
